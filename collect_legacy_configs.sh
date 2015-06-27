728aab511921e51853aa403acaad953458c2f5b4
Mario Juric <mjuric@lsst.org>
2015-06-27 00:09:40 -0700

    Product with .table and .cfg files for external products

---
+++ collect_legacy_configs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PRODUCTS="python swig libevent boost flask twisted scons numpy protobuf matplotlib"
+
+REPOROOT="https://github.com/LSST"
+UPS="ups.new"
+
+mkdir -p "$UPS"
+
+rm -rf tmp
+for prod in $PRODUCTS; do
+	git clone --depth=1 "$REPOROOT/$prod" tmp
+	cp -a tmp/ups/*.{table,cfg} "$UPS"
+	rm -rf tmp
+done
+
+mv ups ups.old
+mv "$UPS" ups
+rm -rf ups.old
